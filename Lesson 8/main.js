71477f4ef3c1fecc6f2ca5e1e5197c9feccbc588
SviatoslavIvanchuk <isv1796@gmail.com>
2025-02-25 15:13:21 +0200

    Lesson 8

--- Lesson 8/main.js
+++ Lesson 8/main.js
@@ -2,21 +2,52 @@ console.log('---------------1---------------');
 // #AiN5CoUQ
 // Створити функцію, яка робить глибоку копію об'єкту.
 // Додати перевірки на undefined, null, NaN.
-//     Подумати і реалізувати логіку, в якій кінцевий об'єкт буде мати функції,які в нього були до цього моменту.
+// Подумати і реалізувати логіку, в якій кінцевий об'єкт буде мати функції,які в нього були до цього моменту.
 
+function deepClone(obj) {
+    if (obj) {
+        let functions = [];
+        for (const key in obj) {
+            if (typeof obj[key] === 'function') {
+                const functionClone = obj[key].bind({});
+                functions.push({functionClone, key});
+            }
+        }
+        const cloneObj = JSON.parse(JSON.stringify(obj));
+        for (const func of functions) {
+            cloneObj[func.key] = func.functionClone;
+        }
+        return cloneObj;
+    }
 
+    throw new Error('Error clone function');
+}
 
+const original = {
+    num: 42,
+    str: "hello",
+    bool: true,
+    method: function () { return "I am a method!"; },
+    nested: {
+        arr: [1, 2, { key: "value" }],
+    },
+    func: function () { return this.num; }
+};
+
+deepClone(original);
 
 console.log('---------------2---------------');
 // #iz6emEsP2BA
 // - є масив
-// let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
 // за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration
 //     Зробити все ВИКЛЮЧНО за допомоги інлайн конструкції
+let newArray = coursesAndDurationArray.map(((course, index) => ({id: index+1, ...course}) ));
+console.log(newArray);
